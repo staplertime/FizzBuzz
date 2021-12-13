57158a3ba252e3283f5e22532e53b12890565e98
staplertime <85749846+staplertime@users.noreply.github.com>
2021-12-13 12:37:59 +1000

    Code Cleanup

--- main.cpp
+++ main.cpp
@@ -2,31 +2,32 @@
 
 int main() {
 
-for (int i = 1; i <= 30; i++) {
+  for (int i = 1; i <= 30; i++) {
 
-int fizz = i % 3;
-int buzz = i % 5;
-int test1 = 0;
-int test2 = 0;
+    int fizz = i % 3;
+    int buzz = i % 5;
+    int test1 = 0;
+    int test2 = 0;
 
-if (fizz == 0) {
-test1 += 1;
-}
-if (buzz == 0) {
-test2 += 1;
-}
-int test3 = test1 + test2;
+    if (fizz == 0) {
+    test1 += 1;
+    }
+    if (buzz == 0) {
+    test2 += 1;
+    }
+    int test3 = test1 + test2;
 
-if (test3 == 2) {
-std::cout << "fizzbuzz\n";
+    if (test3 == 2) {
+    std::cout << "fizzbuzz\n";
+    }
+    else if (test1 == 1) {
+    std::cout << "fizz\n";
+    }
+    else if (test2 == 1) {
+    std::cout << "buzz\n";
+    }
+    else {
+    std::cout << i << "\n";
+    }
+  }
 }
-else if (test1 == 1) {
-std::cout << "fizz\n";
-}
-else if (test2 == 1) {
-std::cout << "buzz\n";
-}
-else {
-std::cout << i << "\n";
-}
-}}
